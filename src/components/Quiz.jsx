6ff3d51d8c4e5ea606ed5e3f7713269af6dce2c4
Joshua Farr <94776350+Joshua-Farr@users.noreply.github.com>
2023-07-23 20:45:06 -0500

    Small formatting changes - rendering issues remain

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useReducer } from "react";
 import QuizQuestion from "./QuizQuestion";
 import {decode} from 'html-entities';
 import { nanoid } from 'nanoid';
@@ -6,8 +6,156 @@ import { nanoid } from 'nanoid';
 
 export default function Quiz(){
 
+    const theQuiz = [
+        {
+            "category": "History",
+            "type": "boolean",
+            "difficulty": "medium",
+            "question": "&quot;I disapprove of what you say, but I will defend to the death your right to say it&quot; is a quote from French philosopher Voltaire.",
+            "correct_answer": "False",
+            "incorrect_answers": [
+                "True"
+            ]
+        },
+        {
+            "category": "History",
+            "type": "multiple",
+            "difficulty": "medium",
+            "question": "What year did the Battle of Agincourt take place?",
+            "correct_answer": "1415",
+            "incorrect_answers": [
+                "1463",
+                "1401",
+                "1422"
+            ]
+        },
+        {
+            "category": "Entertainment: Video Games",
+            "type": "multiple",
+            "difficulty": "medium",
+            "question": "Which game is NOT part of the Science Adventure series by 5pb. and Nitroplus?",
+            "correct_answer": "Occultic; Nine",
+            "incorrect_answers": [
+                "Steins; Gate",
+                "Robotics; Notes",
+                "Chaos; Child"
+            ]
+        },
+        {
+            "category": "History",
+            "type": "multiple",
+            "difficulty": "medium",
+            "question": "In what year did the Berlin Wall fall?",
+            "correct_answer": "1989",
+            "incorrect_answers": [
+                "1987",
+                "1991",
+                "1993"
+            ]
+        },
+        {
+            "category": "Mythology",
+            "type": "multiple",
+            "difficulty": "medium",
+            "question": "What is the name of the Greek god of peaceful deaths?",
+            "correct_answer": "Thanatos",
+            "incorrect_answers": [
+                "Tartarus",
+                "Hades",
+                "Moros"
+            ]
+        }
+    ]
+
+    const theQuiz2 = [
+        {
+            "category": "History",
+            "type": "boolean",
+            "difficulty": "medium",
+            "question": "THSIS IS A NEW QUIZ;I disapprove of what you say, but I will defend to the death your right to say it&quot; is a quote from French philosopher Voltaire.",
+            "correct_answer": "False",
+            "incorrect_answers": [
+                "True"
+            ]
+        },
+        {
+            "category": "History",
+            "type": "multiple",
+            "difficulty": "medium",
+            "question": "THSIS IS A NEW QUIZf Agincourt take place?",
+            "correct_answer": "1415",
+            "incorrect_answers": [
+                "1463",
+                "1401",
+                "1422"
+            ]
+        },
+        {
+            "category": "Entertainment: Video Games",
+            "type": "multiple",
+            "difficulty": "medium",
+            "question": "WTHSIS IS A NEW QUIZience Adventure series by 5pb. and Nitroplus?",
+            "correct_answer": "Occultic; Nine",
+            "incorrect_answers": [
+                "Steins; Gate",
+                "Robotics; Notes",
+                "Chaos; Child"
+            ]
+        },
+        {
+            "category": "History",
+            "type": "multiple",
+            "difficulty": "medium",
+            "question": "In what year did THSIS IS A NEW QUIZl?",
+            "correct_answer": "1989",
+            "incorrect_answers": [
+                "1987",
+                "1991",
+                "1993"
+            ]
+        },
+        {
+            "category": "Mythology",
+            "type": "multiple",
+            "difficulty": "medium",
+            "question": "What is the name of THSIS IS A NEW QUIZ deaths?",
+            "correct_answer": "Thanatos",
+            "incorrect_answers": [
+                "Tartarus",
+                "Hades",
+                "Moros"
+            ]
+        }
+    ]
+
+
+
     const [quizQuestions, setQuizQuestions] = React.useState([])
-    // const [answers, setAnswers] = React.useState()
+
+    const [theQuizState, setTheQuizState] = React.useState(true)
+    const [userScore, setUserScore] = React.useState(0)
+
+    // React.useEffect( () => {
+    //     console.log("Quiz state has changed!")
+    // },[theQuizState])
+
+    function newGame(){
+        console.log("Starting a new game!")
+        setUserScore(0)
+        console.log("Resetting the user score!")
+        setTheQuizState(true)
+        console.log("Setting the user state to true!")
+        console.log("The quiz state is:", theQuizState)
+        fetch("https://opentdb.com/api.php?amount=5&difficulty=medium")
+        .then(res => res.json())
+        .then(data => {
+            setQuizQuestions(data.results)
+            console.log(data.results)
+            console.log(theQuizState)
+        })    
+    }
+
+    const [answers, setAnswers] = React.useState()
 
     React.useEffect( () =>{
         fetch("https://opentdb.com/api.php?amount=5&difficulty=medium")
@@ -15,6 +163,7 @@ export default function Quiz(){
         .then(data => {
             setQuizQuestions(data.results)
             console.log(data.results)
+            console.log(theQuizState)
         })
     },[])
 
@@ -62,9 +211,14 @@ export default function Quiz(){
 
         // Need to add shuffling algorithm
         // console.log(answersArray)
-        return answersArray;
+        return answersArray
     }
 
+    function checkAnswers(){
+        console.log("checking anwers!")
+        setTheQuizState(false)
+
+    }
 
 
     return(
@@ -79,7 +233,6 @@ export default function Quiz(){
                 <QuizQuestion 
                     question={decode(quizQuestions[1].question)} 
                     answers={getAnswers(quizQuestions[1].correct_answer, quizQuestions[1].incorrect_answers)}
-
                 /> 
                 <QuizQuestion 
                     question={decode(quizQuestions[2].question)} 
@@ -95,9 +248,17 @@ export default function Quiz(){
                     answers={getAnswers(quizQuestions[4].correct_answer, quizQuestions[4].incorrect_answers)}
 
                 /> 
+                {/* {!theQuizState ?  
+                <> */}
+                    <h4 className="user-score">You got {userScore}/5 questions right!</h4>
+                    <button onClick={newGame}>New Game</button>
+                {/* </> : */}
+                <button onClick={checkAnswers}>Check Answers</button>
+        }
             </>
             : <p>loading...</p>}
-            <button>Check Answers</button>
+
+            
         </div>
     )
 }
--- src/components/QuizQuestion.jsx
+++ src/components/QuizQuestion.jsx
@@ -7,7 +7,7 @@ export default function QuizQuestion({question, answers}){
 
     const [theAnswers, setTheAnswers] = React.useState(answers)
  
-    React.useEffect( ()=>{
+    React.useEffect( () =>{
         
         console.log("here are the answers updated: ", theAnswers)
     },[theAnswers])
@@ -20,12 +20,12 @@ export default function QuizQuestion({question, answers}){
         console.log("looking for id: ", id)
         setTheAnswers( prevAnswers => prevAnswers.map( answer => {
             return answer.id === id ?
-            {...answer, isSelected: !answer.isSelected} :
+            {...answer, isSelected: true} :
             {...answer, isSelected: false}
 
         } ))
-
-        console.log("Clicked on: ", id)
+        console.log("Answers updated!")
+        // console.log("Clicked on: ", id)
     }
 
     return(
@@ -33,22 +33,22 @@ export default function QuizQuestion({question, answers}){
             <>{question}</>
             <div className="answers-list">
                 <span className="answer" onClick={() => clicked(answers[0].id)} 
-                    style={{backgroundColor: theAnswers[0].isSelected ? "hsla(230, 61%, 90%, 1)" : "white"}}>
+                    style={{backgroundColor: theAnswers[0]?.isSelected ? "hsla(230, 61%, 90%, 1)" : "white"}}>
                     {decode(answers[0].value)} 
                 </span>
                 
                 <span className="answer" onClick={() => clicked(answers[1].id)} 
-                    style={{backgroundColor: theAnswers[1].isSelected ? "hsla(230, 61%, 90%, 1)" : "white"}}>
+                    style={{backgroundColor: theAnswers[1]?.isSelected ? "hsla(230, 61%, 90%, 1)" : "white"}}>
                     {decode(answers[1].value)} 
                 </span>
                 { answers.length > 2 ?
                 <>
                     <span className="answer" onClick={() => clicked(answers[2].id)} 
-                    style={{backgroundColor: theAnswers[2].isSelected ? "hsla(230, 61%, 90%, 1)" : "white"}}>
+                    style={{backgroundColor: theAnswers[2]?.isSelected ? "hsla(230, 61%, 90%, 1)" : "white"}}>
                             {decode(answers[2].value)}
                     </span>
                     <span className="answer" onClick={() => clicked(answers[3].id)} 
-                        style={{backgroundColor: theAnswers[3].isSelected ? "hsla(230, 61%, 90%, 1)" : "white"}}>
+                        style={{backgroundColor: theAnswers[3]?.isSelected ? "hsla(230, 61%, 90%, 1)" : "white"}}>
                             {decode(answers[3].value)}
                     </span>
                 </>
@@ -59,3 +59,13 @@ export default function QuizQuestion({question, answers}){
 
 
 }
+
+
+
+//psedo code for checking answers
+
+//button clicked
+//for each question:
+    //compare user selection to answer
+    //update user score
+//display users score
