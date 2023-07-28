5ff3a67f839e89386fc16441f627b458f2feda49
Joshua Farr <94776350+Joshua-Farr@users.noreply.github.com>
2023-07-28 17:28:45 -0500

    Conditional color rendering for answer selection

--- src/components/QuizQuestion.jsx
+++ src/components/QuizQuestion.jsx
@@ -21,12 +21,22 @@ export default function QuizQuestion({ question, answers, id, selectAnswer }) {
         <span
           className="answer"
           onClick={() => selectAnswer(id, answers[0].id)}
+          style={{
+            backgroundColor: answers[0].isSelected
+              ? "hsla(230, 61%, 90%, 1)"
+              : "white",
+          }}
         >
           {answers[0].answer}
         </span>
         <span
           className="answer"
           onClick={() => selectAnswer(id, answers[1].id)}
+          style={{
+            backgroundColor: answers[1].isSelected
+              ? "hsla(230, 61%, 90%, 1)"
+              : "white",
+          }}
         >
           {answers[1].answer}
         </span>
@@ -35,12 +45,22 @@ export default function QuizQuestion({ question, answers, id, selectAnswer }) {
             <span
               className="answer"
               onClick={() => selectAnswer(id, answers[2].id)}
+              style={{
+                backgroundColor: answers[2].isSelected
+                  ? "hsla(230, 61%, 90%, 1)"
+                  : "white",
+              }}
             >
               {answers[2].answer}
             </span>
             <span
               className="answer"
               onClick={() => selectAnswer(id, answers[3].id)}
+              style={{
+                backgroundColor: answers[3].isSelected
+                  ? "hsla(230, 61%, 90%, 1)"
+                  : "white",
+              }}
             >
               {answers[3].answer}
             </span>
